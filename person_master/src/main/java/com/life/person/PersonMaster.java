6b2aad84c2144f086a26a4ac3c12a3b248993441
Egor Martynov <ekip-s@yandex.ru>
2023-09-24 00:51:33 +0300

    feature: add directory;

---
+++ person_master/src/main/java/com/life/person/PersonMaster.java
@@ -0,0 +1,11 @@
+package com.life.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonMaster {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonMaster.class, args);
+    }
+}
