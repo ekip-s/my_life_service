fde80564473ed142d45642e8a446bea42d7e5394
Egor Martynov <ekip-s@yandex.ru>
2023-09-24 11:11:17 +0300

    feature: add config, and schema.sql;

---
+++ courses_master/src/main/java/com/life/courses/CoursesMaster.java
@@ -0,0 +1,11 @@
+package com.life.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesMaster {
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesMaster.class, args);
+    }
+}
