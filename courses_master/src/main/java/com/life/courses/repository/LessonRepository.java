d8dc3cf21cbb0df8b900e93170664d78fe82e20d
Egor Martynov <ekip-s@yandex.ru>
2023-10-14 16:23:38 +0300

    fix: fix add lesson num;

--- courses_master/src/main/java/com/life/courses/repository/LessonRepository.java
+++ courses_master/src/main/java/com/life/courses/repository/LessonRepository.java
@@ -4,13 +4,11 @@ package com.life.courses.repository;
 import com.life.model.courses.Course;
 import com.life.model.courses.Lesson;
 import com.life.model.courses.Status;
-import com.life.model.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -22,5 +20,6 @@ public interface LessonRepository extends JpaRepository<Lesson, UUID> {
             " order by planedStartDate")
     List<Lesson> getLastLessons();
     List<Lesson> findAllByCourseAndStatusOrderByLessonNum(Course course, Status status);
+    List<Lesson> findAllByCourse(Course course);
     void deleteAllByCourse(Course course);
 }
